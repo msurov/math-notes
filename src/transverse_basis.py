caf2ebe0572249a7e90c2f17f27d3498a283d989
Maksim Surov <maksim@nomagic.ai>
2019-02-15 03:07:33 +0100

    fixes

--- src/transverse_basis.py
+++ src/transverse_basis.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from misc.math import integrate
+from scipy.linalg import expm, logm
 
 
 
 class Curve:
     def __init__(self, dim):
-        self.nfreq = 2
+        self.nfreq = 3
         self.ndim = dim
 
         self.a = np.random.random((self.nfreq, self.ndim)) - 0.5
@@ -92,22 +93,25 @@ def get_basis(v):
     v = np.reshape(v, (-1,1))
     U,l,Vt = np.linalg.svd(v.T)
     d = len(v)
-    basis = Vt / np.linalg.det(Vt)**d
+    basis = Vt / l[0]
+    if np.linalg.det(basis) < 0:
+        basis[:,-1] = -basis[:,-1]
     return basis
 
 
 def test():
     np.random.seed(3)
-    t = np.linspace(0, 4*np.pi, 10000)
-    ndim = 5
+
+    T = 2*np.pi
+    t = np.linspace(0, T, 10000)
+    ndim = 3
     gamma = Curve(ndim)
 
 
     def A(t):
         cur, = gamma.curvature(t)
         d, = gamma.der1(t)
-        k = 0.
-        A = (k * np.eye(ndim) - np.outer(d, cur))
+        A = exter(cur, d)
         return A
 
     def rhs(t,E):
@@ -116,10 +120,87 @@ def test():
 
     tau0, = gamma.tangent(0.)
     E0 = get_perp(tau0)
-    t, E = integrate(rhs, E0, [0, 1], step=1e-3)
+    t, E = integrate(rhs, E0, [0, T], step=1e-3)
     assert np.all(np.abs([gamma.tangent(ti).dot(Ei) for ti,Ei in zip(t,E)]) < 1e-10), 'E is not perpendicular to tangent'
-    assert np.all([np.allclose(np.dot(Ei.T, Ei), np.eye(ndim-1)) for Ei in E]), 'E is not orthonormal' 
+    assert np.all([np.allclose(np.dot(Ei.T, Ei), np.eye(ndim-1)) for Ei in E]), 'E is not orthonormal'
+
+
+def exter(a, b):
+    ab = np.outer(a,b)
+    return ab - ab.T
+
+
+def test2():
+    np.random.seed(3)
+
+    T = 2*np.pi
+    t = np.linspace(0, T, 1000)
+    ndim = 5
+    gamma = Curve(ndim)
+
+
+    def A(t):
+        cur, = gamma.curvature(t)
+        d, = gamma.der1(t)
+        A = exter(cur, d)
+        return A
+
+    def rhs(t,E):
+        return A(t).dot(E)
+
+    tau0, = gamma.tangent(0.)
+    E0 = get_perp(tau0)
+    t, E = integrate(rhs, E0, [0, T], step=1e-3)
+
+    En = E[-1]
+    log_EnE0 = logm(En.T.dot(E0))
+    F = [Ei.dot(expm(ti / T * log_EnE0)) for ti,Ei in zip(t,E)]
+
+    assert np.all(np.abs([gamma.tangent(ti).dot(Fi) for ti,Fi in zip(t,F)]) < 1e-10), 'F is not perpendicular to tangent'
+    assert np.all([np.allclose(np.dot(Fi.T, Fi), np.eye(ndim-1)) for Fi in F]), 'F is not orthonormal'
+
+
+def test3():
+    np.random.seed(3)
+
+    T = 2*np.pi
+    t = np.linspace(0, T, 1000)
+    ndim = 10
+    gamma = Curve(ndim)
+
+
+    def A(t):
+        cur, = gamma.curvature(t)
+        d, = gamma.der1(t)
+        A = exter(cur, d)
+        return A
+
+    def rhs(t,E):
+        return A(t).dot(E)
+
+
+    tau0, = gamma.tangent(0.)
+    R0 = get_basis(tau0)
+    t, R = integrate(rhs, R0, [0, T], step=1e-3)
+
+    assert np.all([abs(np.linalg.det(Ri) - 1) < 1e-6 for Ri in R]), 'R is not a rotation matrix'
+    assert np.all([np.allclose(Ri.T.dot(Ri), np.eye(ndim)) for Ri in R]), 'R is not a rotation matrix'
+
+    Rn = R[-1]
+    log_RnR0 = logm(Rn.T.dot(R0))
+    F = [Ri.dot(expm(ti / T * log_RnR0)) for ti,Ri in zip(t,R)]
+
+    assert np.all([abs(np.linalg.det(Fi) - 1) < 1e-6 for Fi in F]), 'F is not a rotation matrix'
+    assert np.all([np.allclose(Fi.T.dot(Fi), np.eye(ndim)) for Fi in F]), 'F is not a rotation matrix'
+    tmp = np.zeros((1,ndim), dtype=float)
+    tmp[0,0] = 1
+    assert np.all([np.allclose(gamma.tangent(ti).dot(Fi), tmp) for ti,Fi in zip(t,F)]), 'F is not perp to tau'
+
+    F0 = F[0]
+    Fn = F[-1]
+    assert np.allclose(F0.T.dot(Fn), np.eye(ndim)), 'F is not periodic'
 
 
 if __name__ == '__main__':
-    test()
+    np.set_printoptions(suppress=True, linewidth=200)
+    test3()
